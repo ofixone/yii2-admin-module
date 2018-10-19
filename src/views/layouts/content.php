e1af36baab4a7a8b8ef127732601b872f8292c0e
ofixone <ofixmail@yandex.ru>
2018-10-19 18:11:10 +0300

    Добавил виджет хлебных крошек в layout

--- src/views/layouts/content.php
+++ src/views/layouts/content.php
@@ -6,11 +6,16 @@
 
 use yii\widgets\Breadcrumbs;
 use ofixone\admin\widgets\alert\Widget as Alert;
-
 ?>
 <div class="content-wrapper">
     <section class="content-header">
+        <h1><?= $this->title ?></h1>
         <?= Breadcrumbs::widget([
+            'homeLink' => [
+                'encode' => false,
+                'label' => '<i class="fa fa-dashboard"></i>  Главная',
+                'url' => ['admin/module/dashboard']
+            ],
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
     </section>
